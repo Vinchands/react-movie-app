f669044094fd19f4b460f8d79243cacca646b19c
Kevin CS <kevinchandra031@gmail.com>
2025-01-21 14:54:56 +0700

    Modify navbar

--- src/components/ui/Navbar/Navbar.view.tsx
+++ src/components/ui/Navbar/Navbar.view.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react'
+import { Link, NavLink } from 'react-router'
+import { Offcanvas } from '@components/ui'
+
 export default function Navbar() {
+  
+  const [offcanvasOpen, setOffcanvasOpen] = useState(false)
+  
+  function handleOpenOffcanvas() {
+    setOffcanvasOpen(prev => !prev)
+  }
+  
   return (
-    <header className="flex justify-between sm:justify-around items-center p-3 bg-slate-100">
-      Watcha
+    <header className="flex justify-between sm:justify-around items-center p-3">
+      <div>
+        <Link to="/" className="font-bold text-2xl">Watcha.</Link>
+      </div>
+      <nav className="flex items-center gap-x-5">
+        <NavLink to="/" className="hidden sm:inline">Home</NavLink>
+        <NavLink to="/" className="hidden sm:inline">About</NavLink>
+        <NavLink to="/" className="hidden sm:inline">Movies</NavLink>
+        <NavLink to="/" className="hidden sm:inline">TV Shows</NavLink>
+        <div>
+          <button>Menu</button>
+          <Offcanvas open={offcanvasOpen} />
+        </div>
+      </nav>
     </header>
   )
 }
