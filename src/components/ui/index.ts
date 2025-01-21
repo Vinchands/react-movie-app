a855af2eeacccdab0f3e9bfae3a2ae9ae2eb8201
Kevin CS <kevinchandra031@gmail.com>
2025-01-21 14:55:22 +0700

    Add import for navbar

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,5 @@
 import Navbar from './Navbar/Navbar.view'
 import Footer from './Footer/Footer.view'
+import Offcanvas from './Offcanvas/Offcanvas.view'
 
-export { Navbar, Footer }
+export { Navbar, Footer, Offcanvas }
